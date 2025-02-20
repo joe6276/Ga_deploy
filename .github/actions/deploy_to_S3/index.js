896493acecb6bca66ca4a0c199635f2444be616b
Jonathan Ndambuki <joendambuki16@gmail.com>
2025-02-20 10:37:17 +0300

    Working on deploy

---
+++ .github/actions/deploy_to_S3/index.js
@@ -0,0 +1,19 @@
+const core= require('@actions/core') // get inputs and set otputs
+const exec= require('@actions/exec') // interact wththe cli
+
+function run(){
+    
+    //Get inputs
+
+    const bucket = core.getInput('bucketName', {required:true})
+    const bucketRegion = core.getInput('bucketRegion', {required:true})
+    const distFiles = core.getInput('distFiles', {required:true})
+
+    // upload
+    const s3Url= `s3://${bucket}`
+    exec.exec(` aws s3 sync ${distFiles} ${s3Url} --region ${bucketRegion}`)
+    
+
+}
+
+run()
